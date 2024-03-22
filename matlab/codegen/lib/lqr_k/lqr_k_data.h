67449f8084d907ec5aff5c01d4f4be9c3b9df70f
LeeZekai233 <115407059+LeeZekai233@users.noreply.github.com>
2024-03-22 21:37:40 +0800

    add matlab

---
+++ matlab/codegen/lib/lqr_k/lqr_k_data.h
@@ -0,0 +1,24 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ * File: lqr_k_data.h
+ *
+ * MATLAB Coder version            : 23.2
+ * C/C++ source code generated on  : 18-Mar-2024 16:21:49
+ */
+
+#ifndef LQR_K_DATA_H
+#define LQR_K_DATA_H
+
+/* Include Files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#endif
+/*
+ * File trailer for lqr_k_data.h
+ *
+ * [EOF]
+ */
